8b16c11fb81e360612d1199c3190b89901b4eb04
Vitor Mendes <for.vgmm@gmail.com>
2019-07-19 06:43:01 -0300

    Update

--- Desafio-PartnerGroup/App_Start/SqlConfig.cs
+++ Desafio-PartnerGroup/App_Start/SqlConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using Desafio_PartnerGroup.SQL;
@@ -9,10 +10,12 @@ namespace Desafio_PartnerGroup {
 
         public static void Start() {
 
-            new BaseSQL(){
-                Server = "DESKTOP-NNOAUCQ\\SQLEXPRESS",
-                Database = "db-vgmm",
-            }.Connect();
+            BaseSQL.Connect(ConfigurationManager.ConnectionStrings["SQL"].ConnectionString);
+
+            //new BaseSQL(){
+            //    Server = "DESKTOP-NNOAUCQ\\SQLEXPRESS",
+            //    Database = "db-vgmm",
+            //}.Connect();
 
         }
     }
