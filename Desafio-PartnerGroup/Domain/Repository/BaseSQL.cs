ce9af200d677f8366a431f0323bf7635f8bb36e5
Vitor Mendes <for.vgmm@gmail.com>
2019-07-26 09:34:47 -0300

    Erase comments

--- Desafio-PartnerGroup/Domain/Repository/BaseSQL.cs
+++ Desafio-PartnerGroup/Domain/Repository/BaseSQL.cs
@@ -10,21 +10,6 @@ using System.Web;
 namespace Desafio_PartnerGroup.SQL {
     public class BaseSQL {
 
-        public enum eSqlAuthType {
-            SqlAutentication,
-            WindowsAutentication
-        }
-
-        public string DataSource;
-        public string InitialCatalog;
-
-        public string Server;
-
-        public string UserId;
-        public string Password;
-        public eSqlAuthType AuthType;
-
-        public static string ConnectionString;
         public static SqlConnection Connection;
 
         public BaseSQL() {
@@ -44,51 +29,6 @@ namespace Desafio_PartnerGroup.SQL {
                 throw new Exception(ex.Message);
             }
 
-        }
-
-        public void Connect() {
-            Connection = new SqlConnection(BuildConnectionString(true, ConfigurationManager.ConnectionStrings["SQL"].ConnectionString));
-
-            try {
-
-                Connection.Open();
-                Connection.Close();
-
-            } catch (Exception ex) {
-
-            }
-        }
-
-        private static string BuildConnectionString(bool dbConnection, string connectionString) {
-
-            if (!dbConnection) {
-                connectionString = Regex.Replace(connectionString, @"database(.*?);", "" , RegexOptions.IgnoreCase | RegexOptions.IgnorePatternWhitespace);
-                connectionString = Regex.Replace(connectionString, @"initialcatalog(.*?);", "", RegexOptions.IgnoreCase | RegexOptions.IgnorePatternWhitespace);
-            }
-
-
-            //if (Server != null && Database != null) {
-            //    ConnectionString += String.Format("Server={0}; ", Server);
-            //    ConnectionString += String.Format("Database={0}; ", Database);
-            //} else if (InitialCatalog != null && DataSource != null) {
-            //    ConnectionString += String.Format("Data Source={0}; ", DataSource);
-            //    ConnectionString += String.Format("Initial Catalog={0}; ", InitialCatalog);
-            //} else {
-            //    throw new Exception("Configuração de Dados SQL insuficiente!");
-            //}
-
-            //if (UserId != null && Password != null) {
-            //    ConnectionString += String.Format("User Id={0}; ", UserId);
-            //    ConnectionString += String.Format("Password={0}; ", Password);
-            //} else if (InitialCatalog != null && DataSource != null) {
-            //    ConnectionString += String.Format("Integrated Security=SSPI;");
-            //} else if (Server != null && Database != null) {
-            //    ConnectionString += String.Format("Trusted_Connection=True;");
-            //} else {
-            //    throw new Exception("Configuração de Dados SQL insuficiente!");
-            //}
-
-            return connectionString;
         }
 
         public static DataTable Execute(string query) {
